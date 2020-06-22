1391f99b0fedb563349de4856b7a2b78f4cd94f6
Dominic Ferris <ferris.dominic@gmail.com>
2020-06-22 02:33:57 -0500

    create ball class with movement+canvas boundaries, refactor js files

--- javascript/brick-breaker/brick-breaker.js
+++ javascript/brick-breaker/brick-breaker.js
@@ -1,5 +1,6 @@
 import Paddle from "./paddle.mjs";
 import InputHandler from "./input.mjs";
+import Ball from "./ball.js";
 
 const canvas = document.getElementById("brick-breaker-canvas");
 const context = canvas.getContext("2d");
@@ -9,7 +10,9 @@ const GAME_HEIGHT = 600;
 
 // PADDLE
 const paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
-paddle.draw(context);
+
+// BALL
+const ball = new Ball(GAME_WIDTH, GAME_HEIGHT);
 
 // PLAYER INPUT
 new InputHandler(paddle);
@@ -26,7 +29,10 @@ const gameLoop = (timestamp) => {
   paddle.update(deltaTime);
   paddle.draw(context);
 
+  ball.update(deltaTime);
+  ball.draw(context);
+
   requestAnimationFrame(gameLoop);
 };
 
-gameLoop();
+requestAnimationFrame(gameLoop);
--- javascript/brick-breaker/paddle.mjs
+++ javascript/brick-breaker/paddle.mjs
@@ -31,7 +31,6 @@ export default class Paddle {
   }
 
   update(deltaTime) {
-    if (!deltaTime) return;
     this.position.x += this.speed;
     if (this.position.x < 0) this.position.x = 0;
     if (this.position.x + this.width > this.gameWidth)
