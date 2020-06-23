8d9b71a4788ccb97a0d7a02efedb58d9689eba00
Dominic Ferris <ferris.dominic@gmail.com>
2020-06-22 20:02:32 -0500

    add gamestate and pause functionality

--- javascript/brick_breaker/game.js
+++ javascript/brick_breaker/game.js
@@ -4,6 +4,13 @@ import Ball from "./ball.js";
 import Brick from "./brick.js";
 import { buildLevel, level1 } from "./levels.js";
 
+const GAMESTATE = {
+  PAUSED: 0,
+  RUNNING: 1,
+  MENU: 2,
+  GAMEOVER: 3,
+};
+
 export default class Game {
   constructor(gameWidth, gameHeight) {
     this.gameWidth = gameWidth;
@@ -11,6 +18,8 @@ export default class Game {
   }
 
   start() {
+    this.gamestate = GAMESTATE.RUNNING;
+
     // PADDLE
     this.paddle = new Paddle(this);
 
@@ -23,10 +32,12 @@ export default class Game {
     this.gameObjects = [this.paddle, this.ball, ...bricks];
 
     // PLAYER INPUT
-    new InputHandler(this.paddle);
+    new InputHandler(this, this.paddle);
   }
 
   update(deltaTime) {
+    if (this.gamestate == GAMESTATE.PAUSED) return;
+
     this.gameObjects.map((gameObject) => gameObject.update(deltaTime));
 
     this.gameObjects = this.gameObjects.filter(
@@ -36,5 +47,24 @@ export default class Game {
 
   draw(context) {
     this.gameObjects.map((gameObject) => gameObject.draw(context));
+
+    if (this.gamestate == GAMESTATE.PAUSED) {
+      context.rect(0, 0, this.gameWidth, this.gameHeight);
+      context.fillStyle = "rgba(0, 0, 0, 0.2)";
+      context.fill();
+
+      context.font = "30px Arial";
+      context.fillStyle = "white";
+      context.textAlign = "center";
+      context.fillText("paused", this.gameWidth / 2, this.gameHeight / 2);
+    }
+  }
+
+  togglePause() {
+    if (this.gamestate == GAMESTATE.PAUSED) {
+      this.gamestate = GAMESTATE.RUNNING;
+    } else {
+      this.gamestate = GAMESTATE.PAUSED;
+    }
   }
 }
--- javascript/brick_breaker/input.js
+++ javascript/brick_breaker/input.js
@@ -1,16 +1,20 @@
 export default class InputHandler {
-  constructor(paddle) {
+  constructor(game, paddle) {
     document.addEventListener("keydown", (event) => {
       switch (event.keyCode) {
         // MOVEMENT LEFT
-        case 37:
-        case 65:
+        case 37: // ARROW LEFT
+        case 65: // A
           paddle.moveLeft();
           break;
         // MOVEMENT RIGHT
-        case 39:
-        case 68:
+        case 39: // ARROW RIGHT
+        case 68: // D
           paddle.moveRight();
+          break;
+        case 27: // ESC
+        case 32: // SPACEBAR
+          game.togglePause();
           break;
       }
     });
