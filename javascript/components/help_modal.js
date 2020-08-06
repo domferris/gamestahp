bad44371a0eda187f93435076cdec62029c3d6ef
Dominic Ferris <ferris.dominic@gmail.com>
2020-08-06 00:21:13 -0500

    create stylesheets and js components folder for help+modal, adjust file connections throughout

--- javascript/components/help_modal.js
+++ javascript/components/help_modal.js
@@ -1,8 +1,12 @@
+// TOGGLE HELP MODAL
 const helpToggle = document.querySelector('.help-toggle');
 const helpModal = document.querySelector('.help');
+const arrowBack = document.querySelector('.arrow-back');
 
 helpToggle.addEventListener('click', (event) => {
+  // adjusts toggle character based on active status
   const toggleCharacter = helpModal.classList.contains('active') ? '?' : 'x';
   helpToggle.innerHTML = toggleCharacter;
+
   helpModal.classList.toggle('active');
 });
